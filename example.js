de34df1786804258db21e1da8b608516b37098b1
ioanlongjing <ioan.snickersnee@gmail.com>
2016-11-18 13:47:29 +0800

    把括弧改成自己喜歡的樣子

--- example.js
+++ example.js
@@ -1,34 +1,40 @@
 angular.module('ui.bootstrap.demo', ['ngAnimate', 'ngSanitize', 'ui.bootstrap']);
 angular.module('ui.bootstrap.demo').controller('DatepickerDemoCtrl', function ($scope) {
-  $scope.today = function() {
+  $scope.today = function() 
+  {
     $scope.dt = new Date();
   };
   $scope.today();
 
-  $scope.clear = function() {
+  $scope.clear = function() 
+  {
     $scope.dt = null;
   };
 
-  $scope.options = {
+  $scope.options = 
+  {
     customClass: getDayClass,
     minDate: new Date(),
     showWeeks: true
   };
 
   // Disable weekend selection
-  function disabled(data) {
+  function disabled(data) 
+  {
     var date = data.date,
       mode = data.mode;
     return mode === 'day' && (date.getDay() === 0 || date.getDay() === 6);
   }
 
-  $scope.toggleMin = function() {
+  $scope.toggleMin = function() 
+  {
     $scope.options.minDate = $scope.options.minDate ? null : new Date();
   };
 
   $scope.toggleMin();
 
-  $scope.setDate = function(year, month, day) {
+  $scope.setDate = function(year, month, day) 
+  {
     $scope.dt = new Date(year, month, day);
   };
 
@@ -47,16 +53,19 @@ angular.module('ui.bootstrap.demo').controller('DatepickerDemoCtrl', function ($
     }
   ];
 
-  function getDayClass(data) {
+  function getDayClass(data) 
+  {
     var date = data.date,
       mode = data.mode;
     if (mode === 'day') {
       var dayToCheck = new Date(date).setHours(0,0,0,0);
 
-      for (var i = 0; i < $scope.events.length; i++) {
+      for (var i = 0; i < $scope.events.length; i++) 
+      {
         var currentDay = new Date($scope.events[i].date).setHours(0,0,0,0);
 
-        if (dayToCheck === currentDay) {
+        if (dayToCheck === currentDay) 
+        {
           return $scope.events[i].status;
         }
       }
